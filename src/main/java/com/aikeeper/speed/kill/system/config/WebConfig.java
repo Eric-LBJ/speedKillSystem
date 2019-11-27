ae9252285b25b2f420201859140792881e685bd2
Eric <ga.zhang@ailivingtech.com>
2019-11-27 16:47:47 +0800

    踩坑后，添加资源处理器才能加载到静态资源

--- src/main/java/com/aikeeper/speed/kill/system/config/WebConfig.java
+++ src/main/java/com/aikeeper/speed/kill/system/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.aikeeper.speed.kill.system.config;
 import com.aikeeper.speed.kill.system.resolver.SpeedKillUserResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.annotation.Resource;
@@ -21,8 +22,23 @@ public class WebConfig extends WebMvcConfigurationSupport {
     private SpeedKillUserResolver speedKillUserResolver;
 
     @Override
-    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        super.addArgumentResolvers(argumentResolvers);
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(speedKillUserResolver);
     }
+
+    /**
+     * 这里有点踩坑了，springboot2.0中的WebMvcConfigurerAdapter已经被弃用了，所以我选择用WebMvcConfigurationSupport来添加参数解析器
+     * 但是config继承了WebMvcConfigurationSupport之后默认会加载不到静态资源，所以需要加资源处理器
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+        super.addResourceHandlers(registry);
+    }
+
 }
